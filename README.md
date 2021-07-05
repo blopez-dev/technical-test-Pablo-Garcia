fbac91ddc2dda9e586d2461a0d2750327be960ce
blopez-dev <pjgl.frontend@gmail.com>
2021-07-06 00:47:06 +0200

    Finish the project and refined some details.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Technical-test-Pablo-Garcia
 
-Prueba Técnica para Sopra Steria, basada en la api de usuarios reqres.in
+Technical Test for Sopra Steria, based on the reqres.in user api
 
 ## Table of contents
 
@@ -18,14 +18,14 @@ In order to be able to view both the users and to be able to interact with them,
 
 Project is created with:
 
-- React.js: 12.3
-- React Redux: 2.33
-- Thunk
-- Axios
-- Husky
-- jest
-- React Testing Library
-- Styled Components: 999
+- React.js: 17.0.2
+- React-Redux: 7.2.4
+- Redux-Thunk: 2.3.0
+- Axios: 0.21.1
+- Husky: 6.0.0
+- jest: 5.14.1
+- React Testing Library: 11.2.7
+- Styled Components: 5.3.0
 
 ## Setup
 
--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadUsers } from 'application/store/actions/users';
 import { selectUsers } from 'application/store/reducers/users';
-import Listing from '../../common/comoponents/Listing/Listing';
-import Pane from '../../common/comoponents/Pane';
+import Listing from '../../common/components/Listing/Listing';
+import Pane from '../../common/components/Pane';
 import UserDetail from './components/UserDetail/index';
 import UsersLayout from './styles';
 
@@ -26,7 +26,7 @@ const Users = () => {
   }, []);
 
   return (
-    <UsersLayout className="pepeito">
+    <UsersLayout>
       <Listing items={users} onView={onUserView} />
       <Pane
         isOpen={isOpen}
